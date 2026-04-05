de555309cfe12b93812d7ed0bcb412c97f755507
Daniel Tsivkovski <89223402+dtsivkovski@users.noreply.github.com>
2026-04-05 15:03:30 -0700

    update template layout

--- app/exercise/5/page.tsx
+++ app/exercise/5/page.tsx
@@ -5,12 +5,17 @@ export default function ExerciseTwo() {
         {/* Edit the card below */}
         <div className="flex flex-col border border-black max-w-sm">
           <h2>Computer Science Club</h2>
-          <p>Welcome to our club! Our goal is to help CS students meet new people, build and strengthen their skills, and have fun! Below are some cool events we've done:</p>
+          <p>Chapman University</p>
+          <p>Our goal is to help CS students meet new people, build and strengthen their skills, and have fun!</p>
+          <p>Featured Events:</p>
           <ul>
             <li>PantherHacks</li>
             <li>Boolean Bonfire</li>
             <li>Presentation Night</li>
           </ul>
+          <div>
+            <button className="border border-black">Join now!</button>
+          </div>
         </div>
         {/* Don't edit above */}
       </main>
